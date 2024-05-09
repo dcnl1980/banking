97c95c604dc31122630d67b2dcd809dc9fedab45
TheDev <cvsteenbergen@gmail.com>
2024-05-09 18:33:44 +0200

    adding dashboard integration for Plaid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSideBar from '@/components/RightSideBar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = async () => {
-
+const Home = async ({ searchParams: { id, page }}: SearchParamProps) => {
     const loggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({ userId: loggedIn?.$id });
+
+    if(!accounts) return;
+
+    const accountsData = accounts?.data;
+
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+    const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
@@ -14,19 +23,19 @@ const Home = async () => {
                 <HeaderBox 
                 type="greeting"
                 title="Welcome"
-                user={loggedIn?.name || 'Guest'}
+                user={loggedIn?.firstName || 'Guest'}
                 subtext="Access and manage your account and transactions efficiently."
                 />
            
             <TotalBalanceBox 
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={1250.35}
+                accounts={accountsData}
+                totalBanks={accounts?.totalBanks || 0}
+                totalCurrentBalance={accounts?.totalCurrentBalance || 0}
                 />
             </header>
             RECENT TRANSACTIONS 
         </div>
-        <RightSideBar user={loggedIn} transactions={[]} banks={[{ currentBalance: 12  }, { currentBalance: 200  }]} />
+        <RightSideBar user={loggedIn} transactions={accounts?.transactions} banks={accountsData?.slice(0,2)} />
     </section>
   )
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { ID } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 import { createAdminClient, createSessionClient } from "../appwrite"
 import { cookies } from "next/headers"
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils"
@@ -16,6 +16,17 @@ const {
     APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID
 } = process.env;
 
+// Get user information from the database
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+    try {
+        const { database } = await createAdminClient();
+        const user = await database.listDocuments(DATABASE_ID!, USER_COLLECTION_ID!, [Query.equal('userId', [userId])]);
+        return parseStringify(user.documents[0]);
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
+
 export const signIn = async ({ email, password }: signInProps) => {
     try {
       const { account } = await createAdminClient();
@@ -26,9 +37,12 @@ export const signIn = async ({ email, password }: signInProps) => {
         httpOnly: true,
         sameSite: "strict",
         secure: false,
-      });
+      }); 
+
+      // User the session to get the user info
+      const user = await getUserInfo({ userId: session.userId });
   
-      return parseStringify(session);
+      return parseStringify(user);
     } catch (error) {
       console.error('Error', error);
     }
@@ -82,7 +96,10 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
 export async function getLoggedInUser() {
     try {
       const { account } = await createSessionClient();
-      const user = await account.get();
+      const result = await account.get(); 
+
+      const user = await getUserInfo({ userId: result.$id });
+      
       return parseStringify(user);
     } catch (error) {
       console.error('Error', error);
@@ -120,7 +137,7 @@ export const createLinkToken = async (user: User) => {
 }
 
 export const createBankAccount = async ({
- userId, bankId, accountId, accessToken, fundingSourceUrl, sharableId
+ userId, bankId, accountId, accessToken, fundingSourceUrl, shareableId
 }: createBankAccountProps) => {
     try {
         const { database } = await createAdminClient();
@@ -130,7 +147,7 @@ export const createBankAccount = async ({
         accountId,
         accessToken,
         fundingSourceUrl,
-        sharableId
+        shareableId
         });
 
         return parseStringify(bankAccount);
@@ -184,7 +201,7 @@ export const exchangePublicToken = async ({
             accountId: accountData.account_id,
             accessToken,
             fundingSourceUrl,
-            sharableId: encryptId(accountData.account_id)
+            shareableId: encryptId(accountData.account_id)
         })
 
         // Revalidate the Path
@@ -193,6 +210,26 @@ export const exchangePublicToken = async ({
         return parseStringify(
             { publicTokenExchange: "complete" }
         );
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
+
+export const getBanks = async ({ userId }: getBanksProps) => {
+    try {
+        const { database } = await createAdminClient(); 
+        const banks = await database.listDocuments(DATABASE_ID!, BANK_COLLECTION_ID!, [Query.equal('userId', [userId])]);
+        return parseStringify(banks.documents);
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
+
+export const getBank = async ({ documentId }: getBanksProps) => {
+    try {  
+        const { database } = await createAdminClient(); 
+        const bank = await database.listDocuments(DATABASE_ID!, BANK_COLLECTION_ID!, [Query.equal('$id', [documentId])]);
+        return parseStringify(bank.documents[0]);
     } catch (error) {
         console.log('Error', error)
     }
